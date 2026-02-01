c0979009ebe12b6350100f49b0d2465e685e4308
Kruthika-K-2003 <kruthikavaru@gmail.com>
2026-02-01 23:18:59 +0530

    Update next.config.ts to enable production settings and disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/nextjs-mini-proj/' : '',
+  basePath: isProd ? '/nextjs-mini-proj' : '',
+  output: 'export'
 };
 
 export default nextConfig;
